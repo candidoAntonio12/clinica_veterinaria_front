9aaa7daec91c15dece382c30e955cbe6db6f964e
candidoAntonio12 <candidogoncalvesant9@gmail.com>
2026-06-25 01:46:35 +0100

    home page

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,17 +1,23 @@
+import  "./Card.css" 
+import no_image from "/src/assets/no_image.jpeg"
 
 export default function Card(
         {
             title = "consultas" , description = "consulta",
-            height = "auto", width="auto"
+            height = "100%", width="auto", src = no_image,
+            header = "header"
 
         }
     )
 {
     return (
         <>
-            <div>
-                <div className="card-icon">
-                    <img src="" alt="icon" />
+            <div style={{height: height, width:width}} className="card">
+                <div className="header">
+                    <h5>{header}</h5>
+                    <div className="card-icon">
+                        <img src={src} alt="icon" />
+                    </div>
                 </div>
                 <div className="card-title">
                     {title}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,16 @@ import Card from "../components/card/Card";
 import Hero from "../components/layouts/hero/Hero";
 import NavBar from "../components/layouts/navBar/NavBar";
 import "./Home.css"
+import exam  from "../assets/exam-time.png"
+import vacine from "../assets/vacination.png"
+import consulte from "../assets/online-doctor.png"
+
 
 export default function Home()
 {
+    const descriptionConsulta = "Exame Clinico para Cães e gatos com historico sempre presente na ficha do animal";
+    const descriptionVacinacao = "Calendario de vacina acompanhado de perto, com lembrete automatico do proximo reforço";
+    const descriptionExame = "Resultados laboratorios rápidos e confiaveis";
 
     return ( 
     <>
@@ -18,14 +25,29 @@ export default function Home()
             </section>
             <section className="cards">
                     <ul className="items">
-                        <li className="item-1">
-                            <Card height="200px" width="300px"/>
+                        <li className="item item-1">
+                            <Card 
+                                title="Consultas"
+                                header="Avaliação"  
+                                description={descriptionConsulta}
+                                src={consulte}
+                            />
                         </li>
-                        <li className="item-1">
-                            <Card height="200px" width="300px"/>
+                        <li className="item item-2">
+                            <Card 
+                                title="Vacinaçao" 
+                                header="Prevenção" 
+                                description={descriptionVacinacao}
+                                src={vacine}
+                            />
                         </li>
-                        <li className="item-1">
-                            <Card height="200px" width="300px"/>
+                        <li className="item item-3">
+                            <Card
+                                title="Exames" 
+                                header="Diagnostico" 
+                                description={descriptionExame}
+                                src={exam}
+                            />
                         </li>
                     </ul>
             </section>
